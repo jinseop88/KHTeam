9f683907808e82d041bcbe8a4ea0f516a01017d3
kiming88 <kiming88@cf78a9de-ae2e-0510-b830-8e0ea8f17c2e>
2016-03-26 02:37:26 +0000

    테스트 테스트

---
+++ Client/Proto01/Assets/followcamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followcamera : MonoBehaviour {
+
+    public GameObject target;
+    Vector3 prevPos;
+	void Start()
+    {
+        prevPos = transform.position;
+    }
+	void Update () 
+    {
+        //target
+        transform.position = target.transform.position + prevPos;
+
+	}
+}
