42acee97856cdc0830bffaa62da75409096095dd
kiming88 <kiming88@naver.com>
2016-04-24 04:16:58 +0000

    fgbdsaz

---
+++ Client/Proto01/Assets/TestTitle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTitle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UIManager.Instance.Initialize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Client/Proto01/Assets/testGameLobby.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testGameLobby : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UIManager.Instance.OpenUI(eUIType.GameLobby);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
