4e50a92d63afce8bbc0defe84d8c323fedb556cf
kiming88 <kiming88@naver.com>
2016-05-01 13:39:24 +0000

    스킬 관련 코드추가

---
+++ Client/Proto01/Assets/_Sript/Entity/Battle/CharacterBattle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterBattle : Battle 
+{
+    public Character character { get { return (Character)m_owner; } }
+
+    public override void OnDamege() 
+    { 
+
+    }
+
+}
