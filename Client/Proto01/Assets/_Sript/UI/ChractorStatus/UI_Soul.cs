2b18025ce74800610fde9455a979ae12faaa78d5
dhedaa <dhedaa@cf78a9de-ae2e-0510-b830-8e0ea8f17c2e>
2016-05-14 04:21:54 +0000

    2016.05.14 stageScene, chapterScene UI 구축 && CharactorStatus 생성

---
+++ Client/Proto01/Assets/_Sript/UI/ChractorStatus/UI_Soul.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_Soul : Item {
+
+    //public Text soulCount;
+    public int soulItemCount = 0;
+
+    Item soulItem;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (soulItem == true)
+        {
+            soulItemCount++;
+            //soulCount.text = soulItemCount;
+        }
+
+    }
+}
--- Client/Proto01/Assets/_Sript/UI/StageSelector/UI_StageSelector.cs
+++ Client/Proto01/Assets/_Sript/UI/StageSelector/UI_StageSelector.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class UI_StageSelector : UIBase
@@ -8,23 +9,25 @@ public class UI_StageSelector : UIBase
     public UIPage_Stage m_uiStage;
 
 
-    public void ClickChapter(GameObject obj)
+    public void ClickStage(GameObject obj)
     {
 
         switch (obj.name)
         {
-            case "Button_Chapter_1":
-                Debug.Log("1번눌렸어요");
+            case "Button_Stage_1":
+                //Debug.Log("1번눌렸어요");
+                Application.LoadLevel("5.ChapterSelect");
                 break;
-            case "Button_Chapter_2":
-                Debug.Log("2번눌렸어요");
+            case "Button_Stage_2":                
                 break;
-            case "Button_Chapter_3":
-                Debug.Log("3번눌렸어요");
+            case "Button_Stage_3":                
                 break;
-            case "Button_Chapter_4":
-                Debug.Log("4번눌렸어요");
+            case "Button_Stage_4":                
                 break;
+            
+
         }
     }
+
+   
 }
