3441452b643eadc37e1714c2d218352d76b68567
kiming88 <kiming88@cf78a9de-ae2e-0510-b830-8e0ea8f17c2e>
2016-05-22 04:55:54 +0000

    지움

--- Client/Proto01/Assets/_Sript/UI/GameLobby/Page/UIPage_Chapter.cs
+++ Client/Proto01/Assets/_Sript/UI/GameLobby/Page/UIPage_Chapter.cs
@@ -3,10 +3,21 @@ using System.Collections;
 
 public class UIPage_Chapter : UIBase
 {
-
     public void ClickChapter(GameObject obj)
     {
-        //int num = int.Parse(obj.name.Split("Button_Chapter_")[1]);
+        switch(obj.name)
+        {
+            case "Button_Chapter_1":
+                break;
+            case "Button_Chapter_2":
+                break;
+            case "Button_Chapter_3":
+                break;
+            case "Button_Chapter_4":
+                break;
+        }
+
+        SceneManager.Instance.ChangeScene(SceneType.Game);
     }
 
     
