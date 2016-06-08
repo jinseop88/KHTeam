35618ece5fcca934bf95c81ef2ea764598f443d6
kiming88 <kiming88@naver.com>
2016-06-08 15:12:32 +0000

    트랩 구현

--- Client/Proto01/Assets/_Sript/Entity/Environment/Trap.cs
+++ Client/Proto01/Assets/_Sript/Entity/Environment/Trap.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Trap : MonoBehaviour {
+public class Trap : MonoBehaviour 
+{
+    private GameObject m_thisObject;
+    private Transform m_thisTansform;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject thisObject { get { return m_thisObject; } }
+    public Transform thisTransform { get { return m_thisTansform; } }
+
+    void Awake()
+    {
+        m_thisObject = gameObject;
+        m_thisTansform = transform;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Character"))
+        {
+            //체력 닳게
+        }
+    }	
 }
