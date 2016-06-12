39212342459b0474a1bf3f72b7d27e42abcde901
kiming88 <kiming88@naver.com>
2016-06-12 03:11:02 +0000

    콘 업로드

--- Client/Proto01/Assets/_Sript/Entity/Environment/Cone.cs
+++ Client/Proto01/Assets/_Sript/Entity/Environment/Cone.cs
@@ -1,12 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Cone : MonoBehaviour 
+public class Cone : BaseEntity 
 {
-    private Actor m_Target;
+    public float m_fShakeValue;
+    public float m_fSight;
 
-    void Awake()
+    private Actor m_Target;
+    private Vector3 m_firstPos;
+
+    private Rigidbody m_rigidBody;
+
+    private bool m_bStartAction;
+
+    void Start()
     {
-        m_Target = GameObject.FindObjectOfType<Character>();
+        Initialize();
     }
+
+    public override void Initialize()
+    {
+ 	    base.Initialize();
+
+        m_Target = GameObject.FindObjectOfType<Character>();
+        m_rigidBody = gameObject.GetComponent<Rigidbody>();
+
+        m_firstPos = transform.position;
+
+        m_bStartAction = false;
+    }
+    void Update()
+    {
+        if(!m_bStartAction && Mathf.Abs(m_Target.thisTransform.position.x - m_firstPos.x) < m_fSight)
+        {
+            m_bStartAction = true;
+            StartCoroutine(ShakeNDrop());
+        }
+
+        if (Mathf.Abs(m_Target.thisTransform.position.y) > 100)
+            Destroy(thisObject);
+    }
+    IEnumerator ShakeNDrop()
+    {
+        float fTime = 0.2f;
+
+        while (fTime > 0)
+        {
+            
+            fTime -= Time.deltaTime;
+            thisTransform.position = m_firstPos + (Random.insideUnitSphere * m_fShakeValue);
+            yield return null;
+        }
+
+        m_rigidBody.isKinematic = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Character"))
+        {
+            //체력 닳게
+            other.gameObject.GetComponent<Character>().onDamage(this, new SkillImpactInfo());
+        }
+    }	
 }
