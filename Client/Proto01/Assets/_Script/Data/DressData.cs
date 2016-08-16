4d8c7a380ea0eacd1cfe6e0e3182fe997a34bc60
jinseop88 <kiming88@naver.com>
2016-08-16 23:48:14 +0900

    드레스룸 UI 업로드

---
+++ Client/Proto01/Assets/_Script/Data/DressData.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Data
+{
+    public class Dress
+    {
+        public int index;
+
+        public string name;
+        public string dec;
+
+        public int flowerPrice;
+        public int coinPrice;
+
+        public string thumbPath;
+    }
+}
---
+++ Client/Proto01/Assets/_Script/UI/DressRoom/UI_DressRoom.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_DressRoom : UIBase
+{
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+}
