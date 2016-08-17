492a7c0324ef4ac028d4271ebd69deba0e0e0349
Jang daemyung <dm86.jang@gmail.com>
2016-08-18 02:01:28 +0900

    When the touch event is happend, the petal magic is played
    Change-Id: I722cdd69eb52f80ee09653252410dbe122a730a2

---
+++ Client/Proto01/Assets/_Script/Entity/Magic/Magic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// TODO: Replace to Skill
+public class Magic : MonoBehaviour
+{
+    public GameObject magicPrefabs = null;
+    public Transform magicPosition = null;
+
+    private GameObject magic = null;
+    private ParticleSystem particleSystem = null;
+
+    void Update()
+    {
+        if (magic == null && Input.GetMouseButtonDown(0))
+        {
+            magic = Instantiate(magicPrefabs, magicPosition.position, magicPosition.rotation) as GameObject;
+            particleSystem = magic.GetComponentInChildren<ParticleSystem>();
+
+            foreach(GameObject monster in GameObject.FindGameObjectsWithTag("monster"))
+            {
+                monster.GetComponent<Actor>().onDamage(null, null);
+            }
+
+            StartCoroutine(DestroyMagic());
+        }
+    }
+
+    IEnumerator DestroyMagic()
+    {
+        yield return new WaitUntil(() => !particleSystem.isPlaying);
+
+        Destroy(magic);
+        magic = null;
+        particleSystem = null;
+    }
+}
