8b93c67495880a75ea14b45143008fbe8dba6e93
pjyoon316 <pjyoon316@naver.com>
2016-09-05 22:42:41 +0900

    Magictable/Animation추가
    Change-Id: Ia3a9579eeb45b46ac69b2567ec32e9675443af44
    
    Conflicts:
    	Client/Proto01/Assets/Art/Game/BackGround/41_50.meta
    	Client/Proto01/Assets/Art/Game/BackGround/51_60.meta
    	Client/Proto01/Assets/Art/Game/BackGround/51_60/Ground.png.meta
    	Client/Proto01/Assets/Art/Game/BackGround/51_60/Pound.png.meta
    	Client/Proto01/Assets/Art/Game/BackGround/51_60/Sky.png.meta
    	Client/Proto01/Assets/Art/UI/Item/Insam_BackGround.png.meta
    	Client/Proto01/Assets/Art/UI/Item/Insam_Gauge.png.meta
    	Client/Proto01/ProjectSettings/ProjectVersion.txt

--- Client/Proto01/Assets/_Script/Data/DressData.cs
+++ Client/Proto01/Assets/_Script/Data/DressData.cs
@@ -39,31 +39,31 @@ public class DressTable
         m_dressList.Clear(); 
 
         Data.Dress dress = new Data.Dress();
-        dress.index = 1;	dress.name = "Basic";	    dress.dec = "그냥 한복";	        dress.flowerPrice = 0;      dress.coinPrice = 0;
+        dress.index = 1;	dress.name = "무당옷";	    dress.dec = "그냥 한복";	        dress.flowerPrice = 0;      dress.coinPrice = 0;
         m_dressList.Add(dress);
 
         dress = new Data.Dress();
-        dress.index = 2;	dress.name = "GoodDress";	dress.dec = "고급진 양반 한복";	    dress.flowerPrice = 100;    dress.coinPrice = 1000;
+        dress.index = 2;	dress.name = "고급진양반한복";	dress.dec = "고급진 양반 한복";	    dress.flowerPrice = 100;    dress.coinPrice = 1000;
         m_dressList.Add(dress);
 
         dress = new Data.Dress();
-        dress.index = 3;	dress.name = "PinkDress";	dress.dec = "꽃분홍 한복";	        dress.flowerPrice = 200;    dress.coinPrice = 2000;
+        dress.index = 3;	dress.name = "꽃분홍한복";	dress.dec = "꽃분홍 한복";	        dress.flowerPrice = 200;    dress.coinPrice = 2000;
         m_dressList.Add(dress);
 
         dress = new Data.Dress();
-        dress.index = 4;	dress.name = "RoseDress";	dress.dec = "오늘은 황진이 한복";	dress.flowerPrice = 600;    dress.coinPrice = 6000;
+        dress.index = 4;	dress.name = "오늘은황진이한복";	dress.dec = "오늘은 황진이 한복";	dress.flowerPrice = 600;    dress.coinPrice = 6000;
         m_dressList.Add(dress);
 
         dress = new Data.Dress();
-        dress.index = 5;	dress.name = "BlueDress";	dress.dec = "푸르른 남색한복";	    dress.flowerPrice = 300;    dress.coinPrice = 3000;
+        dress.index = 5;	dress.name = "푸르른남색한복";	dress.dec = "푸르른 남색한복";	    dress.flowerPrice = 300;    dress.coinPrice = 3000;
         m_dressList.Add(dress);
 
         dress = new Data.Dress();
-        dress.index = 6;	dress.name = "FlowerDress"; dress.dec = "청순한 벚꽃한복";	    dress.flowerPrice = 400;    dress.coinPrice = 4000;
+        dress.index = 6;	dress.name = "청순한벚꽃한복"; dress.dec = "청순한 벚꽃한복";	    dress.flowerPrice = 400;    dress.coinPrice = 4000;
         m_dressList.Add(dress);
 
         dress = new Data.Dress();
-        dress.index = 7;    dress.name = "PurpleDress"; dress.dec = "신비로운 보라한복";    dress.flowerPrice = 500;    dress.coinPrice = 5000;
+        dress.index = 7;    dress.name = "신비로운보라한복"; dress.dec = "신비로운 보라한복";    dress.flowerPrice = 500;    dress.coinPrice = 5000;
         m_dressList.Add(dress);
     }
     
